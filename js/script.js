9b44bb194e79cb74b05452c4c95c39db22af1089
Enrique Fernandez <44121723+enriquefdz92@users.noreply.github.com>
2022-07-14 19:47:15 -0700

    Remove last names

--- js/script.js
+++ js/script.js
@@ -79,7 +79,7 @@ $(document).ready(function () {
         $("#modal-class-details").modal('show');
     });
     $(document).on('click', '.assistant-img', function (event) {
-        document.getElementById('modal-assistant-name').innerHTML = event.target.dataset.name;
+        document.getElementById('modal-assistant-name').innerHTML = event.target.dataset.name.split(' ')[0];
         document.getElementById('modal-assistant-img').src = event.target.src;
         $("#modal-assistant-detail").modal('show');
     });
